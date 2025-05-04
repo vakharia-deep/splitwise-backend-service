c00e3bc05b985836c4d5f186cd571e84470a046c
Deep <deepvakharia@A-128.local>
2025-05-04 22:31:47 +0530

    Splitwise: Back end API service

--- config/runtime.exs
+++ config/runtime.exs
@@ -32,7 +32,6 @@ if config_env() == :prod do
 
   config :splitwise, Splitwise.Repo,
     url: database_url,
-    database: "railway",
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
 
